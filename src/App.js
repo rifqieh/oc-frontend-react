15f2025a4fb62f20dd43f2d5328f4f5a76c9d119
Rifqi Eka Hardianto <ekarifqi@ymail.com>
2020-03-21 20:18:07 +0700

    change Router to HashRouter

--- src/App.js
+++ src/App.js
@@ -6,22 +6,41 @@ import LoginAdmin from './components/LoginAdmin'
 import Register from './components/Register'
 import Dashboard from './components/Dashboard'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <div>
         <Route path="/" component={Home} exact />
-        <Route path="/login/admin" component={() => <LoginAdmin status="admin" />} />
-        <Route path="/login/teacher" component={() => <Login status="teacher" />} />
-        <Route path="/register/teacher" component={() => <Register status="teacher" />} />
-        <Route path="/login/student" component={() => <Login status="student" />} />
-        <Route path="/register/student" component={() => <Register status="student" />} />
+        <Route
+          path="/login/admin"
+          component={() => <LoginAdmin status="admin" />}
+        />
+        <Route
+          path="/login/teacher"
+          component={() => <Login status="teacher" />}
+        />
+        <Route
+          path="/register/teacher"
+          component={() => <Register status="teacher" />}
+        />
+        <Route
+          path="/login/student"
+          component={() => <Login status="student" />}
+        />
+        <Route
+          path="/register/student"
+          component={() => <Register status="student" />}
+        />
         <Route path="/dashboard" component={Dashboard} />
       </div>
-    </Router>
+    </HashRouter>
   )
 }
 
