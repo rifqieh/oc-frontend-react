12ef1a030ec8dc20ca2d3245157c316bc4031f6e
Rifqi Eka Hardianto <ekarifqi@ymail.com>
2020-03-21 20:26:06 +0700

    Updates

---
+++ precache-manifest.b617d9412088d16abd9250c85d4dadbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b9af1f54cca22894f44df1150be433c",
+    "url": "/oc-frontend-react/index.html"
+  },
+  {
+    "revision": "bff714b363c6db18f875",
+    "url": "/oc-frontend-react/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "419e10802b4d3f5b2e81",
+    "url": "/oc-frontend-react/static/css/main.b4c94c7a.chunk.css"
+  },
+  {
+    "revision": "bff714b363c6db18f875",
+    "url": "/oc-frontend-react/static/js/2.cbeb46d4.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/oc-frontend-react/static/js/2.cbeb46d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419e10802b4d3f5b2e81",
+    "url": "/oc-frontend-react/static/js/main.5ea0c825.chunk.js"
+  },
+  {
+    "revision": "f1e72128910333383870",
+    "url": "/oc-frontend-react/static/js/runtime-main.e669a2bb.js"
+  }
+]);
